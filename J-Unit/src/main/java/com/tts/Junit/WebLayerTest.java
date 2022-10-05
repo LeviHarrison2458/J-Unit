3a7668f1d50cf3b2fd8b4246f44a1874679e5380
leviharrison2458 <leviharrison237@gmail.com>
2022-10-04 22:31:36 -0700

    First

---
+++ J-Unit/src/main/java/com/tts/Junit/WebLayerTest.java
@@ -0,0 +1,14 @@
+package com.tts.Junit;
+
+@WebMvcTest
+public class WebLayerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
+	}
+}
---
+++ J-Unit/src/test/java/com/tts/Junit/JUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.Junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitApplicationTests {
+
+	@Test
+public	void contextLoads() {
+	}
+
+}
